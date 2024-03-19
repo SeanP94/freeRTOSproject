bf084eb66dfa57b65d3b2fba8591f8a14cc2b520
seanperry <seanep94@gmail.com>
2024-03-18 20:21:09 -0700

    Final iteration of the double blinking LED's. Everything is 100% as it should be. Onto more concepts..

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ void toggleLED(void *parameter) {
         vTaskDelay(500 / portTICK_PERIOD_MS);
         gpio_set_level(LED_BLINK_PIN, switcher);
         switcher = 1 - switcher;
+        printf("Task1\n");
     }
 }
 
@@ -24,6 +25,7 @@ void toggleOnBoardLED(void *parameter) {
     // Task flips a LED on and off.
     uint8_t switcher = 0;
     for (;;) {
+        printf("Task2\n");
         vTaskDelay(500 / portTICK_PERIOD_MS);
         gpio_set_level(ON_BOARD_LED_BLINK_PIN, switcher);
         switcher = 1 - switcher;
@@ -34,9 +36,8 @@ void toggleOnBoardLED(void *parameter) {
 void app_main() {
     // Log the startup and get RTOS task variables.
     printf("Hello, Startup!\n");
-    TaskHandle_t xHandler = NULL;
-    BaseType_t xLEDTask;
-    BaseType_t xPrintTask;
+    TaskHandle_t xHandler_LED = NULL;
+    TaskHandle_t xHandler_ONBOARDLED = NULL;
     
     // Configure the GPIO LED Pin
     gpio_config_t ledGpioConfig = {
@@ -48,34 +49,23 @@ void app_main() {
     };
     ESP_ERROR_CHECK(gpio_config(&ledGpioConfig));
 
-    xLEDTask = xTaskCreate(
-        toggleLED,          // Task
-        "LED Toggle Task",  // Name task
-        1000,               // this is the stack size in words.
-        NULL,               // No Parameters
-        2,                  // Low priority
-        &xHandler           // Handler of the tasks
-    );
-
-    xPrintTask = xTaskCreate(
-        toggleOnBoardLED,          // Task
-        "LED OnBoard ToggleTask",  // Name task
-        1000,                      // this is the stack size in words.
+    xTaskCreate(
+        toggleLED,                 // Task
+        "LED Toggle Task",         // Name task
+        configMINIMAL_STACK_SIZE,  // this is the stack size in words.
         NULL,                      // No Parameters
         1,                         // Low priority
-        &xHandler                  // Handler of the tasks
+        &xHandler_LED              // Handler of the tasks
     );
 
-    configASSERT(xHandler);
-    
-    // Run so the other function can run forever.
-    for (;;) {
-        // Use this to stop the watchdog from failing and rebooting.
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
+    xTaskCreate(
+        toggleOnBoardLED,           // Task
+        "LED OnBoard ToggleTask",  // Name task
+        configMINIMAL_STACK_SIZE,  // this is the stack size in words.
+        NULL,                      // No Parameters
+        2,                         // Low priority
+        &xHandler_ONBOARDLED       // Handler of the tasks
+    );
 
-    // Cleanup the tasks.
-    if (xHandler != NULL)
-        vTaskDelete(xHandler); 
     printf("Buh-Bye!\n");
 }
